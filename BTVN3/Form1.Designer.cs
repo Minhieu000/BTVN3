02a734ca1e17954bb372c6a3e4435df4c3847a12
THIS PC <THIS PC@LAPTOP-13UV3BRD>
2024-11-28 22:25:47 +0700

    Add project files.

---
+++ BTVN3/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace BTVN3
+{
+    partial class From
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvStudent = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvStudent
+            // 
+            this.lvStudent.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvStudent.FullRowSelect = true;
+            this.lvStudent.GridLines = true;
+            this.lvStudent.HideSelection = false;
+            this.lvStudent.Location = new System.Drawing.Point(71, 45);
+            this.lvStudent.Name = "lvStudent";
+            this.lvStudent.Size = new System.Drawing.Size(316, 219);
+            this.lvStudent.TabIndex = 0;
+            this.lvStudent.UseCompatibleStateImageBehavior = false;
+            this.lvStudent.View = System.Windows.Forms.View.Details;
+            this.lvStudent.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "LastName";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "FirstName";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Phone";
+            // 
+            // txtLast
+            // 
+            this.txtLast.Location = new System.Drawing.Point(478, 56);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(252, 20);
+            this.txtLast.TabIndex = 1;
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(478, 113);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(252, 20);
+            this.txtFirst.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(478, 170);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(252, 20);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(71, 339);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(95, 40);
+            this.btnThem.TabIndex = 4;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(274, 339);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(113, 40);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(478, 339);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(116, 40);
+            this.btnSua.TabIndex = 6;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // From
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvStudent);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.txtLast);
+            this.Controls.Add(this.txtPhone);
+            this.Name = "From";
+            this.Text = "ListView Demo";
+            this.Load += new System.EventHandler(this.BTVN_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvStudent;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+    }
+}
+
---
+++ BTVN3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTVN3
+{
+    public partial class From : Form
+    {
+        public From()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTVN_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            ListViewItem lvi = new ListViewItem (txtLast.Text);
+            lvi.SubItems.Add(txtFirst.Text);
+            lvi.SubItems.Add (txtPhone.Text);
+            lvStudent.Items.Add(lvi);
+      
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (lvStudent.SelectedItems.Count > 0) 
+            {
+                lvStudent.Items.Remove(lvStudent.SelectedItems[0]); 
+                MessageBox.Show("Đã xóa thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một mục để xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            if (lvStudent.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Hãy chọn một mục để xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            // Xóa mục được chọn
+            foreach (ListViewItem selectedItem in lvStudent.SelectedItems)
+            {
+                lvStudent.Items.Remove(selectedItem); // Xóa dựa trên đối tượng
+            }
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (lvStudent.SelectedItems.Count > 0) 
+            {
+                ListViewItem selectedItem = lvStudent.SelectedItems[0];
+                selectedItem.Text = txtLast.Text; 
+                selectedItem.SubItems[1].Text = txtFirst.Text; 
+                selectedItem.SubItems[2].Text = txtPhone.Text; 
+                MessageBox.Show("Đã sửa thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một mục để sửa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+        private void lvStudent_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvStudent.SelectedItems.Count > 0) 
+            {
+                ListViewItem selected = lvStudent.SelectedItems[0];
+                txtLast.Text = selected.Text; 
+                txtFirst.Text = selected.SubItems[1].Text; 
+                txtPhone.Text = selected.SubItems[2].Text; 
+            }
+        }
+        private void from_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Bạn có muốn thoát?", " Tiêu đề thông báo ",
+                                                   MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void txtFirst_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
